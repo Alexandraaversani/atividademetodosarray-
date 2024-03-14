a4f755c9679c7bc82cd6f3a3ecaae16244750776
alexandraaversani.tds2.senai@gmail.com <alexandraaversani.tds2.senai@gmail.com>
2024-03-14 11:22:50 -0300

    metodo filter

---
+++ atividadepraticaarray.js
@@ -0,0 +1,10 @@
+//metodo filter
+//um array de numeros é declarado e inicializado com os valores
+let numeros = [50, 60, 70, 80, 90];
+//no let numerosMaioresQueSetenta vai acontecer a filtragem dos numeros maiores que 70
+let numerosMaioresQueSetenta =
+numeros.filter(numero => numero > 70);
+//exibe o resultado dos numeros maiores que 70
+console.log(numerosMaioresQueSetenta);
+
+
